0ccadd5eab77aeff0e3b39565b5e56f97587d784
Andres Felipe Dorado <andres.dorado90@gmail.com>
2020-03-13 10:08:25 +0100

    First commit

---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player;
+    public float camera_distance = 30.0f;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / camera_distance);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
