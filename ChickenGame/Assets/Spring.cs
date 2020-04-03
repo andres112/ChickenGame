82d6e145f261eb5121500d0c097bee7de0e5f58e
burkt4 <burkt4@gmail.com>
2020-04-03 14:12:57 +0200

    moving platform and spring objects added

---
+++ ChickenGame/Assets/Spring.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    public Vector2 impulse;
+    public Animator springAnimationController;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider){
+             if (collider.gameObject.tag == "Player")
+             {
+                 Rigidbody2D collider_rigidbody = collider.GetComponent<Rigidbody2D>();
+                 Vector2 velocity = new Vector2(collider_rigidbody.velocity.x,0);
+                 collider_rigidbody.velocity = velocity;
+
+                 collider_rigidbody.AddForce (impulse, ForceMode2D.Impulse);
+                 if(springAnimationController != null){
+                     Debug.Log ("Animation!");
+                    springAnimationController.SetTrigger("active");
+                 }
+             }
+         }
+}
---
+++ ChickenGame/Assets/movingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour
+{
+    public Vector2 direction;
+    private Vector3 fTurningPoint;
+    private Vector3 bTurningPoint;
+    public float speed;
+    public bool forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fTurningPoint = new Vector3 (transform.position.x + direction.x,transform.position.y + direction.y,transform.position.z);
+        bTurningPoint = transform.position;
+        direction.Normalize();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >=  fTurningPoint.x && transform.position.y >=  fTurningPoint.y){
+            forward = false;
+        }
+        if (transform.position.x <= bTurningPoint.x && transform.position.y <=  bTurningPoint.y){
+            forward = true;
+        }
+
+        if(forward){
+            Vector3 newPosition = new Vector3(transform.position.x + speed*direction.x,transform.position.y + speed*direction.y,transform.position.z);
+            transform.position = newPosition;
+        }
+        else{
+            Vector3 newPosition = new Vector3(transform.position.x - speed*direction.x,transform.position.y - speed*direction.y,transform.position.z);
+            transform.position = newPosition;        
+        }
+        
+    }
+}
