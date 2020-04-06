136aeae78118af859676d9fa2cd7673ade34ad5a
Andres Felipe Dorado <andres.dorad@gmail.com>
2020-04-06 21:54:43 +0200

    small fixes in game over execution

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
-{
+public class MovingPlatform : MonoBehaviour {
     public Vector2 direction;
     private Vector3 fTurningPoint;
     private Vector3 bTurningPoint;
@@ -11,32 +10,31 @@ public class MovingPlatform : MonoBehaviour
     public bool forward;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        fTurningPoint = new Vector3 (transform.position.x + direction.x,transform.position.y + direction.y,transform.position.z);
+    void Start () {
+        fTurningPoint = new Vector3 (transform.position.x + direction.x, transform.position.y + direction.y, transform.position.z);
         bTurningPoint = transform.position;
-        direction.Normalize();
+        direction.Normalize ();
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.x >=  fTurningPoint.x && transform.position.y >=  fTurningPoint.y){
-            forward = false;
-        }
-        if (transform.position.x <= bTurningPoint.x && transform.position.y <=  bTurningPoint.y){
-            forward = true;
+    void Update () {
+        if (!GameManager.IsGameOver) {
+            if (transform.position.x >= fTurningPoint.x && transform.position.y >= fTurningPoint.y) {
+                forward = false;
+            }
+            if (transform.position.x <= bTurningPoint.x && transform.position.y <= bTurningPoint.y) {
+                forward = true;
+            }
+
+            if (forward) {
+                Vector3 newPosition = new Vector3 (transform.position.x + speed * direction.x, transform.position.y + speed * direction.y, transform.position.z);
+                transform.position = newPosition;
+            } else {
+                Vector3 newPosition = new Vector3 (transform.position.x - speed * direction.x, transform.position.y - speed * direction.y, transform.position.z);
+                transform.position = newPosition;
+            }
         }
 
-        if(forward){
-            Vector3 newPosition = new Vector3(transform.position.x + speed*direction.x,transform.position.y + speed*direction.y,transform.position.z);
-            transform.position = newPosition;
-        }
-        else{
-            Vector3 newPosition = new Vector3(transform.position.x - speed*direction.x,transform.position.y - speed*direction.y,transform.position.z);
-            transform.position = newPosition;        
-        }
-        
     }
 }
