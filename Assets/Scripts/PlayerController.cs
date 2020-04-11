7dc3fea0bc100bb45164d58fd6b8099deda37997
Andres Felipe Dorado <andres.dorad@gmail.com>
2020-04-11 08:49:14 +0200

    remove blank spaces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     [SerializeField] private Transform pfDoubleJumpEffect;
     //This create an object for RigidBody2D
     private Rigidbody2D rigid;
@@ -46,55 +45,47 @@ public class PlayerController : MonoBehaviour
     // 4 - NewLifeSound
     public string[] soundNames;
 
-    private void Awake()
-    {
+    private void Awake () {
         airJumpCountMax = 2;
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
         //set anim to our animator
-        anim = GetComponent<Animator>();
-        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator> ();
+        rigid = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate () {
         //set our vertical Speed
-        anim.SetFloat("vSpeed", rigid.velocity.y);
+        anim.SetFloat ("vSpeed", rigid.velocity.y);
         //set our grounded bool
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
         //set ground in our Animator to match grounded
-        anim.SetBool("Ground", grounded);
+        anim.SetBool ("Ground", grounded);
         // get horizontal movement
-        float move = Input.GetAxis("Horizontal"); //Gives us of one if we are moving via the arrow keys
+        float move = Input.GetAxis ("Horizontal"); //Gives us of one if we are moving via the arrow keys
         //move our Players rigidbody
 
         // The movement is only restrited to right
-        if (move >= 0)
-        {            
-            if (grounded)
-            {
+        if (move >= 0) {
+            if (grounded) {
                 // validate if ground in platform or floor
-                float variationSpeed = IsPlatform ? AccelerationSpeed: GroundSpeed;
-                rigid.velocity =  new Vector2((variationSpeed + move) * maxSpeed, rigid.velocity.y);
-            }
-            else{
+                float variationSpeed = IsPlatform ? AccelerationSpeed : GroundSpeed;
+                rigid.velocity = new Vector2 ((variationSpeed + move) * maxSpeed, rigid.velocity.y);
+            } else {
                 // Jumping x velocity
-                rigid.velocity = new Vector2((SkySpeed + move) * maxSpeed, rigid.velocity.y);
+                rigid.velocity = new Vector2 ((SkySpeed + move) * maxSpeed, rigid.velocity.y);
             }
-        }
-        else
-        {
-            rigid.velocity = new Vector2((InertiaSpeed + Mathf.Abs(move)), rigid.velocity.y);
+        } else {
+            rigid.velocity = new Vector2 ((InertiaSpeed + Mathf.Abs (move)), rigid.velocity.y);
         }
 
-        Debug.Log("Velocity: "+rigid.velocity);
+        Debug.Log ("Velocity: " + rigid.velocity);
 
         //set our speed
-        anim.SetFloat("Speed", Mathf.Abs((1)));
+        anim.SetFloat ("Speed", Mathf.Abs ((1)));
 
         ////if we are moving left but not facing left flip, and vice versa
         //if (move < 0 && !facingLeft)
@@ -107,44 +98,34 @@ public class PlayerController : MonoBehaviour
         //}
     }
 
-    void Update()
-    {
+    void Update () {
 
-        if (grounded)
-        {
+        if (grounded) {
             airJumpCount = 0;
             // canDoubleJump = true;
         }
 
         //if we are on the ground and the space bar was pressed, change our ground state and add an upward force
         // 
-        if (Input.GetKey(KeyCode.Space))
-        {
+        if (Input.GetKey (KeyCode.Space)) {
 
-            if (grounded)
-            {
+            if (grounded) {
                 // Normal Jump
-                anim.SetBool("Ground", false);
+                anim.SetBool ("Ground", false);
                 rigid.velocity = Vector2.up * jumpvelocity;
                 // rigid.AddForce(new Vector2(0, jumpForce));
 
                 // Sound When player jump
-                theGameManager.managePlaySound(soundNames[1]);
-            }
-            else
-            {
-                if (Input.GetKeyDown(KeyCode.Space) & airJumpCount < airJumpCountMax & ScoreScript.scoreValue >= 3)
-                {
+                theGameManager.managePlaySound (soundNames[1]);
+            } else {
+                if (Input.GetKeyDown (KeyCode.Space) & airJumpCount < airJumpCountMax & ScoreScript.scoreValue >= 3) {
                     // Air Jump
-                    anim.SetBool("Ground", false);
-                    if (airJumpCount == 1)
-                    {
-                        Instantiate(pfDoubleJumpEffect, transform.position, Quaternion.identity);
+                    anim.SetBool ("Ground", false);
+                    if (airJumpCount == 1) {
+                        Instantiate (pfDoubleJumpEffect, transform.position, Quaternion.identity);
                         rigid.velocity = Vector2.up * (jumpvelocity - 2f);
                         ScoreScript.scoreValue--;
-                    }
-                    else
-                    {
+                    } else {
                         rigid.velocity = Vector2.up * (jumpvelocity - 1f);
                         ScoreScript.scoreValue = ScoreScript.scoreValue - 2;
                     }
@@ -157,74 +138,63 @@ public class PlayerController : MonoBehaviour
     }
 
     // detect collision and trigger destroy element (corn)
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
+    public void OnTriggerEnter2D (Collider2D collision) {
         // item collider
-        if (collision.gameObject.layer == 12)
-        {
-            if (collision.gameObject.tag == "Corn")
-            {
+        if (collision.gameObject.layer == 12) {
+            if (collision.gameObject.tag == "Corn") {
                 ScoreScript.scoreValue++; // increase the score every time collide with a corn
-                theGameManager.managePlaySound(soundNames[0]);
+                theGameManager.managePlaySound (soundNames[0]);
             }
-            if (!(collision.gameObject.tag == "Stationary")){
-                collision.gameObject.SetActive(false);
-                Destroy(collision.gameObject); // destroy the item wich collides
+            if (!(collision.gameObject.tag == "Stationary")) {
+                collision.gameObject.SetActive (false);
+                Destroy (collision.gameObject); // destroy the item wich collides
             }
-            
 
         }
     }
 
     // Detect collision
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void OnCollisionEnter2D (Collision2D collision) {
         // Platform Collision
         IsPlatform = false;
-        if(collision.gameObject.layer == 10){
+        if (collision.gameObject.layer == 10) {
             // by default the platforms give to the player more speed
             IsPlatform = true;
         }
         // Enemy collider
-        if (collision.gameObject.layer == 11)
-        {
-            if (collision.gameObject.tag == "Deadly Hazard")
-            {
-                theGameManager.restartGame();
+        if (collision.gameObject.layer == 11) {
+            if (collision.gameObject.tag == "Deadly Hazard") {
+                theGameManager.restartGame ();
                 // Sound When eat corn
-                theGameManager.managePlaySound(soundNames[2]);
+                theGameManager.managePlaySound (soundNames[2]);
             }
             // Wolf collider and restart score
-            if (collision.gameObject.tag == "Damaging Hazard")
-            {
-                theGameManager.respawnGame();
-                theGameManager.managePlaySound(soundNames[2]);
+            if (collision.gameObject.tag == "Damaging Hazard") {
+                theGameManager.respawnGame ();
+                theGameManager.managePlaySound (soundNames[2]);
             }
-        }       
+        }
 
-        if(collision.gameObject.layer == 12){            
+        if (collision.gameObject.layer == 12) {
             // Shield collision
-            if (collision.gameObject.tag == "Shield" & Health.shield < Health.health)
-            {
+            if (collision.gameObject.tag == "Shield" & Health.shield < Health.health) {
                 // increase the shield every time collide with a blue hearth if the shield is less than health
-                Health.shield++; 
-                theGameManager.managePlaySound(soundNames[3]);   
-                             
+                Health.shield++;
+                theGameManager.managePlaySound (soundNames[3]);
+
             }
             // New Life Collision
-            if (collision.gameObject.tag == "Life")
-            {
+            if (collision.gameObject.tag == "Life") {
                 Health.health++; // increase the shield every time collide with a blue hearth
-                theGameManager.managePlaySound(soundNames[4]);                
+                theGameManager.managePlaySound (soundNames[4]);
             }
-            collision.gameObject.SetActive(false);
-            Destroy(collision.gameObject); // destroy the item wich collides
+            collision.gameObject.SetActive (false);
+            Destroy (collision.gameObject); // destroy the item wich collides
         }
     }
 
     //flip if needed
-    void Flip()
-    {
+    void Flip () {
         facingLeft = !facingLeft;
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
