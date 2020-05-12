5200f873c019175922a58ad98793d6727acb14ce
Narek <Narek.Andreasyan@unifr.ch>
2020-05-12 23:36:31 +0200

    - Eagle follow player - Eagle Prefab - Eagle in section_D

---
+++ Assets/EnemyFollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    private Transform target;
+    public float moveSpeed = 3f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = this.GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+   /*
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    */   
+    }
+    private void FixedUpdate() {
+        
+        if (transform.position.x < target.position.x)
+        {
+            transform.localScale = new Vector3(-4,4,0);
+        }
+        else
+        {
+            transform.localScale = new Vector3(4,4,0);
+        }
+        
+        //moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction){
+        // rb.MovePosition((Vector2)transform.position + direction * moveSpeed * Time.deltaTime);
+    }
+}
