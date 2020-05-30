daf885b68be784cf83785ba5beea0a176310a00d
khadijahina <56772767+khadijahina@users.noreply.github.com>
2020-05-31 00:23:12 +0200

    adding script
    adding script

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Vector3 fTurningPoint;
+    private Vector3 bTurningPoint;
+    private Vector3 uTurningPoint;
+    public Vector2 delta;
+    public Vector2 speed;
+    public bool forward;
+    public bool upward;
+    //public float jumpForce;
+    //public LayerMask groundLayers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        fTurningPoint = new Vector3(transform.position.x + delta.x, transform.position.y, transform.position.z);
+        uTurningPoint = new Vector3(transform.position.x, transform.position.y + delta.y / 2, transform.position.z);
+        bTurningPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.IsGameOver && !PauseMenu.GameIsPaused)
+        {
+            if (transform.position.x >= fTurningPoint.x)
+            {
+                forward = false;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (transform.position.x <= bTurningPoint.x)
+            {
+                forward = true;
+                //Rotate  at new Position
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            if (transform.position.y >= uTurningPoint.y)
+            {
+                upward = false;
+            }
+            else if (transform.position.y <= fTurningPoint.y)
+            {
+                upward = true;
+            }
+
+            if (forward)
+            {
+                if (upward)
+                {
+                    Vector3 newPosition = new Vector3(transform.position.x + speed.x , transform.position.y + speed.y * 2, transform.position.z);
+                    transform.position = newPosition;
+                }
+                else
+                {
+                    Vector3 newPosition = new Vector3(transform.position.x + speed.x, transform.position.y - speed.y * 2, transform.position.z);
+                    transform.position = newPosition;
+                }
+                // if (upward)
+                //{
+                //    Vector3 newPosition = new Vector3(transform.position.x + speed.x , transform.position.y + speed.y * 2, transform.position.z);
+                //    transform.position = newPosition;
+                //}
+
+            }
+            else
+            {
+                if (upward)
+                {
+                    Vector3 newPosition = new Vector3(transform.position.x - speed.x, transform.position.y + speed.y * 2, transform.position.z);
+                    transform.position = newPosition;
+                }
+                else
+                {
+                    Vector3 newPosition = new Vector3(transform.position.x - speed.x, transform.position.y - speed.y * 2, transform.position.z);
+                    transform.position = newPosition;
+                }
+            }
+        }
+        // rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
