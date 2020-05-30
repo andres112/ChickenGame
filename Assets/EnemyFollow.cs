44246ffe8eb0cfcbf573b07f3f14be854018fd76
Narek <Narek.Andreasyan@unifr.ch>
2020-05-31 01:48:19 +0200

    - Eagle in sections X,Y,Z

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -8,37 +8,53 @@ public class EnemyFollow : MonoBehaviour {
     public float moveSpeed = 3f;
     private Rigidbody2D rb;
     private Vector2 movement;
-    private List<int> list = new List<int> () { 0, 1, 2, 4, 5, 11, 12 };
-    private Transform flag;
+    private List<int> list = new List<int> () { 0, 1, 2, 4, 5, 12 };
+    public Transform sectionstart;
+    public Transform sectionend;
+    private bool colliderhit = true;
+    public float chaseRadius;
+
 
 
     // Start is called before the first frame update
     void Start(){
         rb = this.GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-       // flag = GameObject.FindGameObjectWithTag("checkpoint_section").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      //  if(flag.position.x >= target.position.x)
-      //  {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-    /*    }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, flag.position, moveSpeed * Time.deltaTime);
 
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
         }
-        */
-        /*
-             Vector3 direction = target.position - transform.position;
-             float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-             rb.rotation = angle;
-             direction.Normalize();
-             movement = direction;
-         */   
+
+        /*       RaycastHit2D sectionstarthit = Physics2D.Raycast(sectionstart.position, Vector2.zero);
+               RaycastHit2D sectionendhit = Physics2D.Raycast(sectionend.position, Vector2.zero);
+   
+               if (sectionendhit.collider == null)
+               {
+   
+                       transform.position =
+                           Vector2.MoveTowards(transform.position, sectionstart.position, moveSpeed * Time.deltaTime);
+                       Debug.Log(sectionendhit.collider.name);
+      
+     
+               }
+               else
+               {
+   
+               }
+   */
+            /*
+                 Vector3 direction = target.position - transform.position;
+                 float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+                 rb.rotation = angle;
+                 direction.Normalize();
+                 movement = direction;
+             */
     }
     private void FixedUpdate() {
         
