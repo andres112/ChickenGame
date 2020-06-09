c78e378427b7dc013513979c6897043bc3d1d309
Narek <Narek.Andreasyan@unifr.ch>
2020-06-09 23:07:22 +0200

    - Eagle in Section D and H

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFollow : MonoBehaviour {
-
+public class EnemyFollow : MonoBehaviour
+{
     private Transform target;
     public float moveSpeed = 3f;
-    private float tempspeed = 1f;
-
     private Rigidbody2D rb;
+
     //private Vector2 movement;
-    private List<int> list = new List<int> () { 0, 1, 2, 4, 5, 12 };
+    private List<int> list = new List<int>() {0, 1, 2, 4, 5, 12};
+
     //public Transform sectionstart;
     //public Transform sectionend;
     public Transform sightStart;
@@ -19,12 +19,14 @@ public class EnemyFollow : MonoBehaviour {
     public LayerMask SectionEndCollider;
 
     private bool colliderHit_forPatrol;
+    private bool colliderHit_forSectionEnd;
     private bool follow = true;
     private bool patrol = true;
     public float chaseRadius;
-    
+
     // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         //rb.isKinematic = true;
@@ -41,9 +43,8 @@ public class EnemyFollow : MonoBehaviour {
 
     void Update()
     {
-
-        bool colliderHit_forSectionEnd = Physics2D.Linecast(sightStart.position, sightEnd.position, SectionEndCollider);
-       //Debug.Log ("**** Chicken follow" + follow);
+        colliderHit_forSectionEnd = Physics2D.Linecast(sightStart.position, sightEnd.position, SectionEndCollider);
+        //Debug.Log ("**** Chicken follow" + follow);
 
         if (colliderHit_forSectionEnd)
         {
@@ -52,10 +53,7 @@ public class EnemyFollow : MonoBehaviour {
 
         if (!follow)
         {
-            
-            transform.localScale = new Vector2 (Mathf.Abs(transform.localScale.x )* -1, transform.localScale.y);
-            //moveSpeed = -moveSpeed;
-            //rb.velocity = new Vector2((-1) * moveSpeed, rb.velocity.y);
+            transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y);
             transform.position =
                 Vector2.MoveTowards(transform.position, target.position, moveSpeed * (-1) * Time.deltaTime);
         }
@@ -66,58 +64,39 @@ public class EnemyFollow : MonoBehaviour {
                 patrol = false;
                 transform.position =
                     Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-
             }
             else
             {
-                //patrol = true;
                 transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
                 colliderHit_forPatrol = Physics2D.Linecast(sightStart.position, sightEnd.position, ColliderPatroling);
 
                 if (colliderHit_forPatrol)
                 {
-
                     transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
                     moveSpeed *= -1;
-
                 }
             }
         }
-
-        /*
-             Vector3 direction = target.position - transform.position;
-             float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-             rb.rotation = angle;
-             direction.Normalize();
-             movement = direction;
-         */
     }
 
-    private void FixedUpdate() {
-
-        
+    private void FixedUpdate()
+    {
         if (transform.position.x < target.position.x)
         {
-            transform.eulerAngles = new Vector3(0,180,0);
+            transform.eulerAngles = new Vector3(0, 180, 0);
         }
         else
         {
-            transform.eulerAngles = new Vector3(0,0,0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        
-        //moveCharacter(movement);
+    }
 
-    }
-    
-    
-    private void OnCollisionEnter2D (Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         // Avoid collisions with platforms
-        if (list.Contains (other.gameObject.layer)) {
-            Physics2D.IgnoreCollision (other.gameObject.GetComponent<Collider2D> (), GetComponent<Collider2D> ());
+        if (list.Contains(other.gameObject.layer))
+        {
+            Physics2D.IgnoreCollision(other.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
-    }
-    
-    void moveCharacter(Vector2 direction){
-        rb.MovePosition((Vector2)transform.position + direction * moveSpeed * Time.deltaTime);
     }
 }
